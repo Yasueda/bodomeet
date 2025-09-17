e90c5df18e2abd753f6ba0ce1dbf22942d0e9a76
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-09-17 05:03:57 +0000

    Fix user_icon

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,8 +28,8 @@ class User < ApplicationRecord
 
   def get_user_image(width, height)
     unless user_image.attached?
-      file_path = Rails.root.join('app/assets/images/users/no_user_image.png')
-      user_image.attach(io: File.open(file_path), filename: 'user-icon.jpg', content_type: 'image/jpg')
+      file_path = Rails.root.join('app/assets/images/users/no_user_image.jpeg')
+      user_image.attach(io: File.open(file_path), filename: 'user-icon.jpeg', content_type: 'image/jpeg')
     end
     user_image.variant(resize_to_limit: [width, height]).processed
   end
