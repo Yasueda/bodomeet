8804b01d202474e88f59304dab9bac69411e9db7
xxxx <217292364+Yasueda@users.noreply.github.com>
2025-10-18 10:58:15 +0000

    Add user-groups-view

--- app/helpers/public/members_helper.rb
+++ app/helpers/public/members_helper.rb
@@ -2,4 +2,9 @@ module Public::MembersHelper
   def member_group?(group_id)
     Member.where(user_id: current_user.id, group_id: group_id).exists?
   end
+
+  def is_approval_member?(group_id, user_id)
+    member = Member.find_by(group_id: group_id, user_id: user_id)
+    return member.is_approval
+  end
 end
