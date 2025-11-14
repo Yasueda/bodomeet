4fc8b83322a58b2e2b0a7eb2138de59653f01270
xxxx <217292364+Yasueda@users.noreply.github.com>
2025-11-14 02:05:05 +0000

    Add RSpec/comment

--- spec/system/user/after_login_spec.rb
+++ spec/system/user/after_login_spec.rb
@@ -5,6 +5,8 @@ describe 'ログインしている場合' do
   let!(:other_user) { create(:user) }
   let!(:event) { create(:event, user: user) }
   let!(:other_event) { create(:event, user: other_user) }
+  let!(:comment) { create(:comment, user: user, event: event)}
+  let!(:other_comment) { create(:comment, user: other_user, event: event)}
 
   before do
     visit new_user_session_path
@@ -75,8 +77,8 @@ describe 'ログインしている場合' do
         expect(page).to have_content other_event.name
       end
       it 'イベントの詳細リンク先がそれぞれ表示される' do
-        expect(page).to have_link event.name, href: admin_event_path(event)
-        expect(page).to have_link other_event.name, href: admin_event_path(other_event)
+        expect(page).to have_link event.name, href: event_path(event)
+        expect(page).to have_link other_event.name, href: event_path(other_event)
       end
     end
   end
@@ -155,13 +157,61 @@ describe 'ログインしている場合' do
 
     context '削除リンクのテスト' do
       before do
-        click_link '削除'
+        all('a', text: '削除').first.click
       end
       it '正しく削除される' do
         expect(Event.where(id: event.id, is_active: true).count).to eq 0
       end
       it 'リダイレクト先が、イベント一覧になっている' do
         expect(current_path).to eq '/events'
+      end
+    end
+
+    context 'コメントのテスト' do
+      it 'コメント入力フォームが表示される' do
+        expect(page).to have_field 'comment[content]'
+      end
+      it '正しく表示される' do
+        expect(page).to have_content comment.content
+        expect(page).to have_content other_comment.content
+      end
+      it 'コメントボタンが存在する' do
+        expect(page).to have_css '#comment-button'
+      end
+
+      context 'コメント追加' do
+        before do
+          @old_comments = event.comments.count
+          fill_in 'comment[content]', with: Faker::Lorem.characters(number: 10)
+          find('#comment-button').click
+        end
+
+        it '新しいコメントが正しく保存される' do
+          expect(event.comments.count).not_to eq @old_comments
+        end
+        it 'リダイレクト先が、コメントした投稿の詳細画面になっている' do
+          expect(current_path).to eq '/events/' + Comment.last.event.id.to_s
+        end
+        it '新しいコメントが正しく表示されている' do
+          expect(page).to have_content Comment.last.content
+        end
+        it 'コメント削除ボタンが存在する' do
+          expect(page).to have_css '#del-comment' + Comment.last.id.to_s
+        end
+      end
+
+      context 'コメント削除' do
+        before do
+          @comment_id = comment.id.to_s
+          @event_id = comment.event.id.to_s
+          find('#del-comment' + @comment_id).click
+        end
+        it '正しく削除される' do
+          expect(Comment.where(id: @comment_id, is_active: true).count).to eq 0
+        end
+        it 'リダイレクト先が、コメントを削除した投稿の詳細画面になっている' do
+          expect(current_path).to eq '/events/' + @event_id
+        end
       end
     end
   end
